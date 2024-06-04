6a8a6c78d19eb7b6787ea1408c2066387f799900
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2024-06-04 16:09:48 -0300

    buscaseleniumcurso

--- BuscaSeleniumPuro.java
+++ BuscaSeleniumPuro.java
@@ -1,1 +1,25 @@
-TESTE
+public class BuscaSeleniumPuro{
+    String url = 'https://iterasys.com.br/';
+    WebDriver driver;
+
+    @Before
+    public void iniciar(){
+        driver = new ChromeDriver();
+        // Timeout implícito
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILISECONDS)
+        // Evita que tenha outra tela na frente
+        driver.manage().window().maximize()
+    }
+
+    @After
+    public void finalizar(){
+        driver.quit()
+    }
+
+    @Test
+    public void buscacurso(){
+        driver.get(url) // acessa o URL
+        driver.findElement(By.id('searchtext')).sendKeys('TestLink' + Keys.ENTER); // Acha o campo de busca, digita a busca e aperta enter
+        driver.findElement(By.cssSelector('span.comprar')).click(); // acha o botão compra e realiza a compra
+    }  
+}
