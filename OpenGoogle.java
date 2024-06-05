70dab0f52a10d3da2ad7e9ea29cbb66ef2d7193c
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2024-06-05 15:44:20 -0300

    exercise1

--- OpenGoogle.java
+++ OpenGoogle.java
@@ -1,3 +1,26 @@
-Exercise 1:
+//Exercise 1:
+//Write a script to open google.com and verify that title is Google and also verify that it is redirected to https://www.google.com.br/
 
-Write a script to open google.com and verify that title is Google and also verify that it is redirected to google.co.in
+public class Exercises {
+
+    @Before
+    public void iniciar(){
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void finalizar(){
+        driver.quit();
+    }
+
+    @Test
+    public void exercise1(){
+        driver.get('www.google.com');
+
+        String title = driver.getTitle();
+        String url = driver.getCurrentUrl();
+
+        AssertEquals('Google', title);
+        AssertTrue(driver.getCurrentUrl().equals("www.google.com.br"));
+    }
+}
