b7daee15b4d5829f0cc57084f537a0863b05f1de
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2024-06-06 08:31:09 -0300

    exercise2

--- OpenGoogle.java
+++ OpenGoogle.java
@@ -35,21 +35,21 @@ public class Exercises {
     try {
         buttonContent = driver.findElement(By.cssSelector("span.Button-content"));
     } catch (NoSuchElementException e) {
-        // Faça algo caso o elemento não seja encontrado, como imprimir uma mensagem de erro ou lançar uma exceção
+        System.out.println('elemento button-content não encontrado')
     }
 
     if (buttonContent != null) {
-        buttonContent.click(); // "click()" com "c" minúsculo
+        buttonContent.click(); 
 
         WebElement buttonNewRepo = null;
         try {
-            buttonNewRepo = driver.findElement(By.xpath("//*[@id=\":r11:--label\"]"));
+            buttonNewRepo = driver.findElement(By.xpath('//*[@id=\":r11:--label\"]'));
         } catch (NoSuchElementException e) {
-            // Faça algo caso o elemento não seja encontrado, como imprimir uma mensagem de erro ou lançar uma exceção
+            System.out.println('elemento novo repositório não encontrado')
         }
 
         if (buttonNewRepo != null) {
-            buttonNewRepo.click(); // "click()" com "c" minúsculo
+            buttonNewRepo.click(); 
         }
     } else {
         fazerLogin();
